11598edd23ca2db566b8b552a2141db2f0fc6315
Hye jun <vxggangxv@naver.com>
2017-01-24 17:50:33 +0900

    결제페이지 추가수정

--- m/js/payment_m.js
+++ m/js/payment_m.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 	
+	$("#contents .term input[type=radio]").on('click', function() {
+		if( $(this).is("checked") ){
+			$(this).closest("ul").addClass("on");	
+		} else {
+			$(this).closest("div").children("ul").removeClass("on");
+			$(this).closest("ul").addClass("on");	
+		}
+	});
 });
