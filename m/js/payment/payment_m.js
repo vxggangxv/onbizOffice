b15591a920015ce44cec1a7b86eb4692568f1468
Hye jun <vxggangxv@naver.com>
2017-01-25 20:31:02 +0900

    경로 수정

--- m/js/payment/payment_m.js
+++ m/js/payment/payment_m.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+	/*라디오박스 스타일 설정*/
 	$("#contents .term input[type=radio]").on('click', function() {
 		if( $(this).is("checked") ){
 			$(this).closest("ul").addClass("on");	
@@ -7,5 +7,9 @@ $(document).ready(function() {
 			$(this).closest("div").children("ul").removeClass("on");
 			$(this).closest("ul").addClass("on");	
 		}
+	});
+	/*체크박스 스타일 설정*/
+	$("#contents .add input[type=checkbox] + label").on("click", function() {
+		$(this).closest("ul").toggleClass("on");
 	});
 });
