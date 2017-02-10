dec657ede197f46381dc0ea7d6997f549bef90de
Hye jun <vxggangxv@naver.com>
2017-02-10 10:15:26 +0900

    결제페이지완료

--- src/js/info.js
+++ src/js/info.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
+  /*셀렉트박스 기능 생성*/
   $("#contents .selectBox .s_btn").on("click", function() {
+    $(this).next().next().slideToggle('fast');
+  });
+  $("#contents .selectBox i").on("click", function() {
     $(this).next().slideToggle('fast');
   });
-  $("#contents .category li").on("click", function() {
+  $("#contents .category > ul > li").on("click", function() {
     var thIdx = $(this).index();
     $("#container > section").css("display", "none");
     $("#container > section").eq(thIdx).css("display", "block");
+    $("#contents .selectBox ul").hide();
   });
+  $("#contents .selectBox ul li").on("click",function(){
+    var thVar = $(this).text();
+    console.log(thVar);
+    $("#contents .selectBox input").val(thVar);
+    $("#contents .selectBox input").attr("type", "button");
+    $(this).closest("ul").slideUp("fast");
+  });
+  $("#contents .selectBox ul li:last").on("click",function(){
+    $("#contents .selectBox input").val("");
+    $("#contents .selectBox input").attr("type", "text");
+    $("#contents .selectBox input").focus();
+    $(this).closest("ul").slideUp("fast");
+  });
+  
   
 });
