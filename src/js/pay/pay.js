c0e723708c6295d975cd591a872ddabcae85f215
Hye jun <vxggangxv@naver.com>
2017-02-13 12:31:27 +0900

    결제페이지 추가수정

--- src/js/pay/pay.js
+++ src/js/pay/pay.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
   $("#contents .term .table .t_radio").on("click",function() {
     $(this).closest("ul").children().removeClass("on");
     $(this).closest("li").addClass("on");
+    //$(this).closest("li").prev().css("border-right", "none");
   });
   /*체크박스 효과 기능*/
   $("#contents .option .o_check").on("click", function() {
     $(this).closest("li").toggleClass("on");
+  });
+  /*카드 할부 선택 기능*/
+  $("#contents .way .co_check").on("click", function() {
+    var thIdx = $(this).closest("li").index();
+    $(this).closest("ul").children().removeClass("on");
+    $(this).closest("li").addClass("on");
+    $(this).closest("ul").next().children().removeClass("on");
+    $(this).closest("ul").next().children().eq(thIdx).addClass("on");
   });
 });
